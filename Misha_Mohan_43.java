3250f913b41de3cbaa6fd8c693aca01dccf828d4
misha9 <36028936+misha9@users.noreply.github.com>
2019-03-27 20:41:02 +0530

    Update Misha_Mohan_43.java

--- Misha_Mohan_43.java
+++ Misha_Mohan_43.java
@@ -1,46 +1,39 @@
 import java.util.Scanner;
-
-public class DiamondPattern {
-             public static void main(String[] args) {
-
-                 System.out.println("enter the no.of lines = ");
-                 Scanner sc = new Scanner(System.in);
-	
-	int lines = sc.nextInt();
-	int mid=(lines+1)/2;
-
-	for (int i=1 ; i<=mid ; i++)
-
-                   { for ( int s=1 ; s<= (mid-i) ; s++)
-                        { System.out.print(" ");
-                            }
-                     for (int j=1 ; j<=(i*2-1) ; j++)
-                           {
-                if(i>(n+1)/2)
-                {
-                    if (j>(n+1)/2&&j<=(2*i-1-((n+1)/2)))
-                        System.out.print(" ");
-                    else
-                        System.out.print("1");
-                }
-                              }
-		System.out.println();
-                      }
-                for (int i=1 ; i<=mid-1 ; i++)
-
-                   { for ( int s=1 ; s<= i ; s++)
-                        { System.out.print(" ");
-                            }
-                     for (int j=1 ; j<= 2*(mid-i)-1 ; j++)
-                          {
-                if(i>(n+1)/2)
-                {
-                    if (j>(n+1)/2&&j<=(2*i-1-((n+1)/2)))
-                        System.out.print(" ");
-                    else
-                        System.out.print("1");
-                }
-                              }
-		System.out.println();
-                      } }
-
+public class Main{
+public static void main(String[] args) {
+    System.out.println("enter the no.of lines = ");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int mid=(2*n+1)/2;
+    for (int i=1 ; i<=mid ; i++)
+    { for ( int s=1 ; s<= (mid-i) ; s++)
+    {   System.out.print(" ");
+    }
+    for (int j=1 ; j<=(i*2-1) ; j++)
+    {
+        if(i<=(2*n+1)/2)
+        {
+        if (j>(n)/2&&j<=(2*i-1-((n)/2)))
+            System.out.print(" ");
+        else
+            System.out.print("1");
+        }
+    }
+    System.out.println();
+    }
+    for (int i=1 ; i<=mid-1 ; i++)
+    {   for ( int s=1 ; s<= i ; s++)
+    {       System.out.print(" ");
+    }
+        for (int j=1 ; j<= 2*(mid-i)-1 ; j++)
+    {
+        if(i<(2*n+1)/2)
+        {
+            if (j>(n)/2&&j<=(2*(mid-i)-1-((n)/2)))
+                System.out.print(" ");
+            else
+                System.out.print("1");
+        }
+    }
+    System.out.println();
+} }}
